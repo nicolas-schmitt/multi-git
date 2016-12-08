1c05a352f040485fce9aef17ee8a3563aab12aac
Nicolas Schmitt <nicolas.schmitt@amundi.com>
2016-12-08 15:25:56 +0100

    fixed library exports

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,13 @@
-import * as Client from './client';
+import * as Errors from './errors';
+import Client from './client';
 import Directory from './directory';
 import Group from './group';
 import Manager from './manager';
 
 module.exports = {
+    Client,
     Directory,
+    Errors,
     Group,
     Manager,
-    Client
 };
